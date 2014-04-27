77e7605509501ad42b07c5225707dded0eb751cd
Jeff Baker <jeff.baker.22@gmail.com>
2014-04-27 13:34:22 -0700

    updated cachematrix.R with assignment edits

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R enables us to calculate, cache, and retrieve 
+## a matrix and it's inverse to prevent recalculation.
 
-## Write a short comment describing this function
+## makeCacheMatrix() sets a matrix in cache memory for quick 
+## retrieval and calculation. 
 
 makeCacheMatrix <- function(x = matrix()) {
+  # clear value of m
+  m <- NULL
+  
+  # "sets" x (higher level environment) to the inputted value
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  # defined function to retrieve (get) x
+  get <- function() x
+  
+  # solves the matrix inverse and stores it as m
+  solveInv <- function(solve) m <<- solve
+  
+  # retrieves the inverse if available
+  getInv <- function() m
 
+  # list of available operators. set/get matrix, and 
+  # set/get inverse solution
+  list(set = set, get = get, 
+       solveInv = solveInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  # set m to the matrix set in makeCacheMatrix()
+  m <- x$getInv()
+  # if there is already a value in m, just retrieve that instead
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  # if there is no inverse-m calculated in makeCacheMatrix, 
+  # then we solve it here and return the value
+  data <- x$get()
+  m <- solve(data, ...)
+  x$solveInv(m)
+  m
 }
+
+## example matrix: x <- matrix(c(2,4,2,2),2)
+## inverse should be: (-0.5,1.0,0.5,-0.5)
